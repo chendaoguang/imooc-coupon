b40e804cdeb2b2d7458033157926e4889b113fa2
chendaoguang <13291860918@163.com>
2020-03-05 00:35:15 +0800

    网关服务

--- coupon-gateway/src/main/java/com/imooc/coupon/ZuulGatewayApplication.java
+++ coupon-gateway/src/main/java/com/imooc/coupon/ZuulGatewayApplication.java
@@ -1,16 +1,19 @@
 package com.imooc.coupon;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * ClassName: ZuulGatewayApplication
  * Package: com.imooc.coupon
+ * @SpringCloudApplication 组合了 SpringBoot 应用 + 服务发现 +熔断
  * description: 网管应用启动入口
  * Date: 2020/3/3 23:59
  * Author: chendaoguang
  */
-@SpringBootApplication
+@EnableZuulProxy
+@SpringCloudApplication
 public class ZuulGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulGatewayApplication.class, args);
